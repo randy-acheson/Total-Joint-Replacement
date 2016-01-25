755e892231f44464cfde74934adcdc90fc3a1603
jnallard <jnallard@wpi.edu>
2016-01-25 17:54:31 -0500

    Added new activities

--- app/src/main/java/edu/wpi/total_joint_replacement/entities/PhysicalAction.java
+++ app/src/main/java/edu/wpi/total_joint_replacement/entities/PhysicalAction.java
@@ -22,10 +22,26 @@ public class PhysicalAction {
     public static PhysicalAction newAction = new PhysicalAction("Add a new activity!", "Press this button to add a new activity to your routine.", "");
 
     public static List<PhysicalAction> getAllActivities(){
+        /*
+        walking (inside or outside; ask separately)
+        biking (stationery/inside or outside)
+        swimming
+        strength and aerobic classes (yoga, dance, etc)
+        golfing
+        gardening, yard work
+        jogging
+                */
+
         List<PhysicalAction> newList = new ArrayList<>(activities);
-        newList.add(new PhysicalAction("Stair Climbing", "Climb those stairs!", "5 steps"));
-        newList.add(new PhysicalAction("Walking", "Walk those paths!", ""));
-        newList.add(new PhysicalAction("Something else", "Do that something else!", "Just do it."));
+        newList.add(new PhysicalAction("Walking (Inside)", "Use that treadmill!", "3 miles"));
+        newList.add(new PhysicalAction("Walking (Outside)", "Walk those paths!", "3 miles"));
+        newList.add(new PhysicalAction("Biking (Inside)", "Use that bike in the gym!", "5 miles"));
+        newList.add(new PhysicalAction("Biking (Outside)", "Bike along those paths!", "5 miles"));
+        newList.add(new PhysicalAction("Swimming", "Go for a dip!", "5 laps"));
+        newList.add(new PhysicalAction("Strength and Aerobic Classes", "Yoga, dance, etc!", "30 minutes"));
+        newList.add(new PhysicalAction("Golfing", "Just keep swinging!", "Play for an hour"));
+        newList.add(new PhysicalAction("Gardening and Yardwork", "Beautify your landscape!", ""));
+        newList.add(new PhysicalAction("Jogging", "Get that heard rate going!", "2 miles"));
         newList.add(newAction);
         return newList;
     }
